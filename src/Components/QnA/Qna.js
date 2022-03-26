17ad063f8008b44e197d5ae6bb305deaddc6dd1f
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-03-26 23:24:40 +0600

    last-commit

--- src/Components/QnA/Qna.js
+++ src/Components/QnA/Qna.js
@@ -5,7 +5,7 @@ const Qna = () => {
     return (
         <div>
             <div className='answer'>
-                <h2>Question: How React Works</h2>
+                <h2>Question: How React Works?</h2>
                 <h3>Answer:</h3>
                 <p>React is a efficient JavaScript library for building user interfaces. The Real DOM represents the entire UI of the web applications. The Real DOM virtual representation is called Virtual DOM. React uses virtual DOM to enhance its performance. Virtual DOM stores a representation of the UI in memory. Whenever something is change in the state/props, a new virtual DOM will be created and it will be compared the Real DOM. React only changes what needs to be changed.  </p>
 
@@ -37,7 +37,7 @@ const Qna = () => {
          </div>
 
          <div className='answer'>
-            <h2>Question: How useState() works</h2>
+            <h2>Question: How useState() works?</h2>
             <h3>Answer:</h3>
             <p>React have two types of components, one is class components and other is functional components. The useState() is a hook that allows state variables in functional components. The useState() hook takes an initial state. It returns variables and function. Variable is a initial state. Function is used for update initial state. </p>
          </div>
